b4598354f8056b6a5b9b18869310c5642c58078e
Henny2 <henrieke.baunack@web.de>
2024-01-31 20:31:53 -0800

    adding tab view example with nav stack INSIDE

--- Project16Playground/ContentView.swift
+++ Project16Playground/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab: String = "list"
+    var body: some View {
+        TabView(selection: $selectedTab)
+        {
+            ListView().tabItem {
+                Label("List View", systemImage: "list.bullet")
+            }
+            NavigationStack{
+                Text("Second tab")
+                    .navigationTitle("Second taaaaab")
+            }
+            .tabItem {
+                Label("number 2", systemImage: "2.circle.fill")
+            }
+        }
+    }
+}
+
+struct ListView: View {
+    let names = ["Johanna", "Florian", "James", "Amy"]
+    @State private var selection: String?
+    // has to be an empty set here, cannot be an optional set
+    @State private var multipleSelection: Set<String> = Set()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            List(names, id: \.self, selection: $selection){ name in
+//                Text(name)
+//            }
+            
+            List(names, id: \.self, selection: $multipleSelection){ name in
+                Text(name)
+            }
+            EditButton()
         }
         .padding()
     }
