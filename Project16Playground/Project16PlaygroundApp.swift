af48b278b1ea51cc0231695f76e51917efec32cd
Henny2 <henrieke.baunack@web.de>
2024-01-31 19:34:29 -0800

    Initial Commit

---
+++ Project16Playground/Project16PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project16PlaygroundApp.swift
+//  Project16Playground
+//
+//  Created by Henrieke Baunack on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Project16PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
